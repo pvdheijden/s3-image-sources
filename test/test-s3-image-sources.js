d326c82fc1f9eaf83b69ecabef083bd4ae14fd8a
BuildTools <unconfigured@null.spigotmc.org>
2015-12-23 17:47:48 +0100

    region config added

--- test/test-s3-image-sources.js
+++ test/test-s3-image-sources.js
@@ -2,6 +2,10 @@
 
 var debug = require('debug')('s3-image-sources');
 
-require('../s3-image-sources.js')('tiles-demo', 'assets', 'image-sources.json', function(err) {
+require('../s3-image-sources.js')('tiles-demo', 'images', 'image-sources.json', function(err) {
+    if (err) {
+        console.error(err);
+    }
+
     debug('done');
 });
