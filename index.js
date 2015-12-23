5829794f291cb982dce972ec206b94d94cd54cce
BuildTools <unconfigured@null.spigotmc.org>
2015-12-23 17:22:03 +0100

    images prefix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ AWS.config.update({
 
 exports.handler = function(event, context) {
     var bucket = event.Records[0].s3.bucket.name;
-    var prefix = 'assets';
+    var prefix = 'images';
     var object = 'image-sources.json';
 
     require('./s3-image-sources.js')(bucket, prefix, object, context.done);
